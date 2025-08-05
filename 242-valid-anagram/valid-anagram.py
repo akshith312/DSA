e18bde77ffa6838d31176c0c0eab61222661bd1b
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-05 17:25:15 -0500

    Time: 19 ms (70.65%) | Memory: 14.5 MB (10.16%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        n = len(s)
+        m = len(t)
+
+        if len(s)!=len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(n):
+            countS[s[i]] = 1 + countS.get(s[i],0) #get(val,defVal) ensures if no occurence it is created and then read
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        
+        return True
