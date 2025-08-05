8a51e8afbafbc47205895fdd926418608d2931d4
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-05 17:35:04 -0500

    Time: 26 ms (30.59%) | Memory: 13.4 MB (41.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,20 +5,22 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        n = len(s)
-        m = len(t)
+        # n = len(s)
+        # m = len(t)
 
-        if len(s)!=len(t):
-            return False
+        # if len(s)!=len(t):
+        #     return False
         
-        countS, countT = {}, {}
+        # countS, countT = {}, {}
 
-        for i in range(n):
-            countS[s[i]] = 1 + countS.get(s[i],0) #get(val,defVal) ensures if no occurence it is created and then read
-            countT[t[i]] = 1 + countT.get(t[i],0)
+        # for i in range(n):
+        #     countS[s[i]] = 1 + countS.get(s[i],0) #get(val,defVal) ensures if no occurence it is created and then read
+        #     countT[t[i]] = 1 + countT.get(t[i],0)
         
-        for c in countS:
-            if countS[c] != countT.get(c,0):
-                return False
+        # for c in countS:
+        #     if countS[c] != countT.get(c,0):
+        #         return False
         
-        return True
+        # return True
+
+        return sorted(s) == sorted(t)
