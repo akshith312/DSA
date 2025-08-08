0c679fb522c4a1c76c95e835d5295d6c2c73eff3
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-08 10:11:10 +0530

    Time: 30 ms (33.36%) | Memory: 18.3 MB (16.10%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,13 +4,26 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        res = defaultdict(list)
-        for s in strs:
-            sortedS = ''.join(sorted(s))
-            res[sortedS].append(s)
+        #brute Force
+        # res = defaultdict(list)
+        # for s in strs:
+        #     sortedS = ''.join(sorted(s))
+        #     res[sortedS].append(s)
 
-        return(list(res.values()))
+        # return(list(res.values()))
     
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord('a')]+=1
+            
+            res[tuple(count)].append(s)
+
+        return list(res.values())
+
 
 
     
