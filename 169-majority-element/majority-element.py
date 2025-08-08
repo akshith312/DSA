e882673812ef00560a9317975336033b87804e48
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-08 22:14:04 +0530

    Time: 10 ms (54.04%) | Memory: 13.6 MB (64.38%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,18 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count = 0
-        maxel = 0
+        #brute force
+        # count = 0
+        # maxel = 0
 
-        for i in range(len(nums)):
-            if count == 0:
-                maxel = nums[i]
-                count +=1
-            elif nums[i] == maxel:
-                count+=1
-            else:
-                count-=1
-        return maxel
+        # for i in range(len(nums)):
+        #     if count == 0:
+        #         maxel = nums[i]
+        #         count +=1
+        #     elif nums[i] == maxel:
+        #         count+=1
+        #     else:
+        #         count-=1
+        # return maxel
+
+        count = defaultdict(int) 
+        elem = 0
+        maxc = 0
+
+        for num in nums:
+            count[num]+= 1
+            if maxc < count[num]:
+                elem = num
+                maxc = count[num]
+        return elem
+
 
 
         
