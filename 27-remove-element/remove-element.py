fed42410de81bfc515a185c0d8e468184c98d64c
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-08 21:05:30 +0530

    Time: 1 ms (10.49%) | Memory: 12.4 MB (51.39%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,6 +5,21 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        #brute force
+
+        # tmp = []
+
+        # for i in range(len(nums)):
+        #     if nums[i] == val:
+        #         continue
+        #     tmp.append(nums[i])
+        
+        # for i in range(len(tmp)):
+        #     nums[i] = tmp[i]
+
+        # return len(tmp)
+
+
         k = 0
 
         for i in range(len(nums)):
