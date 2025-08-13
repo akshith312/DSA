47610196cc2b635860e868f13afda71b6dde32c4
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-13 16:06:40 -0500

    Time: 7 ms (63.23%) | Memory: 12.5 MB (53.95%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -5,34 +5,48 @@ class Solution(object):
         :rtype: bool
         """
         #brute force
-        for row in range(9):
-            seen = set()
-            for i in range(9):
-                if board[row][i]=='.':
-                    continue
-                if board[row][i] in seen:
-                    return False
-                seen.add(board[row][i])
-        for col in range(9):
-            seen = set()
-            for i in range(9):
-                if board[i][col]=='.':
-                    continue
-                if board[i][col] in seen:
-                    return False
-                seen.add(board[i][col])
-        for square in range(9):
-            seen = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square // 3) * 3 + i
-                    col = (square % 3) * 3 + j
+        # for row in range(9):
+        #     seen = set()
+        #     for i in range(9):
+        #         if board[row][i]=='.':
+        #             continue
+        #         if board[row][i] in seen:
+        #             return False
+        #         seen.add(board[row][i])
+        # for col in range(9):
+        #     seen = set()
+        #     for i in range(9):
+        #         if board[i][col]=='.':
+        #             continue
+        #         if board[i][col] in seen:
+        #             return False
+        #         seen.add(board[i][col])
+        # for square in range(9):
+        #     seen = set()
+        #     for i in range(3):
+        #         for j in range(3):
+        #             row = (square // 3) * 3 + i
+        #             col = (square % 3) * 3 + j
 
-                    if board[row][col] == '.':
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
+        #             if board[row][col] == '.':
+        #                 continue
+        #             if board[row][col] in seen:
+        #                 return False
+        #             seen.add(board[row][col])
+        # return True
+
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        square = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[(r//3,c//3)]:
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                square[(r//3,c//3)].add(board[r][c])
         return True
-
         
