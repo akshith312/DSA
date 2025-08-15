ca47f1886fe0f690e76bc71afda6fb406d578682
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-15 15:17:35 -0500

    Time: 22 ms (83.69%) | Memory: 12.7 MB (98.46%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -4,14 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_count = 0
-        maxel = -1
+        #brute force
+        # max_count = 0
+        # maxel = -1
 
-        for num in nums:
-            if num % 2 == 0:
-                count = sum(1 for i in nums if i == num)
-                if count > max_count or (count == max_count and num < maxel):
-                    max_count = count
-                    maxel = num
-        return maxel 
+        # for num in nums:
+        #     if num % 2 == 0:
+        #         count = sum(1 for i in nums if i == num)
+        #         if count > max_count or (count == max_count and num < maxel):
+        #             max_count = count
+        #             maxel = num
+        # return maxel 
+
+        seen = {}
+
+        for item in nums:
+            if item % 2 ==0 :
+                seen[item] = 1 if item not in seen else seen[item] +1
+        max = 0
+        output = -1
+
+        for num, count in seen.items():
+            if count > max:
+                max = count
+                output = num
+            elif count == max:
+                output = min(num, output)
+        return output
         
