b327ba9d2d633aa8dcb4658b3b7c7a1685c9ba06
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-19 13:43:15 -0500

    Time: 37 ms (31.86%) | Memory: 13.5 MB (44.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,14 +4,37 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        #brute force
         s = ''.join(c for c in s if c.isalnum())
         s = s.lower()
-        s1 = s[::-1]
-        s1 = s1.lower()
+        # s1 = s[::-1]
+        # s1 = s1.lower()
         
-        if s == s1:
-            return True
-        return False
+        # if s == s1:
+        #     return True
+        # return False
+
+        l = 0
+        r = len(s) -1
+
+        while l<r:
+            while l<r and not self.alphaNum(s[l]):
+                l+=1
+            while r>l and not self.alphaNum(s[r]):
+                r-=1
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+
+
+    def alphaNum(self,c):
+        return(ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+
+
         
             
 
