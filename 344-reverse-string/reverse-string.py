b79a7aedbc6f4fb10b2ec21b8abb6171218a5221
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-19 10:57:07 -0500

    Time: 0 ms (100.00%) | Memory: 19.8 MB (65.50%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,12 +4,21 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        temp = []
-        for i in range(len(s)-1,-1,-1):
-            temp.append(s[i])
-        for j in range(len(s)):
-            s[j] = temp[j]
-        return s
+        #brute force
+        # temp = []
+        # for i in range(len(s)-1,-1,-1):
+        #     temp.append(s[i])
+        # for j in range(len(s)):
+        #     s[j] = temp[j]
+        # return s
+
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l+=1
+            r-=1
+
             
 
         
