d8d1ba72b819035b144c50575b06f2962c98bb52
Akshith <33996844+akshith312@users.noreply.github.com>
2025-08-26 12:30:44 -0500

    Time: 8 ms (39.36%) | Memory: 22.3 MB (93.82%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,11 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        res = [0] * n
+        #extra space
+        # n = len(nums)
+        # res = [0] * n
 
-        for i in range(n):
-            res[(i+k)%n] = nums[i]
-        nums[:] = res
+        # for i in range(n):
+        #     res[(i+k)%n] = nums[i]
+        # nums[:] = res
+        n = len(nums)
+        k = k%n
+
+        def rev(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        rev(0,n-1)
+        rev(0,k-1)
+        rev(k,n-1)
         
         
