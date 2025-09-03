6222b9c2456712623761af3717f9a5aec7292279
Akshith <33996844+akshith312@users.noreply.github.com>
2025-09-02 19:21:01 -0500

    Time: 23 ms (59.53%) | Memory: 13.1 MB (54.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,14 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        #brute force
+        # leng = 0
+        # for i in range(len(s)):
+        #     subset = set()
+        #     for j in range(i,len(s)):
+        #         if s[j] in subset:
+        #             break
+        #         subset.add(s[j])
+        #     leng = max(leng, len(subset))
+        # return leng
         
-        leng = 0
-        for i in range(len(s)):
-            subset = set()
-            for j in range(i,len(s)):
-                if s[j] in subset:
-                    break
-                subset.add(s[j])
-            leng = max(leng, len(subset))
-        return leng
-        
+        charSet = set()
+        l = 0
+        res = 0 
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r-l+1)
+        return res
