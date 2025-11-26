5cdcbfed266ca68c41b632808a4bee1f08fccc3c
Akshith <33996844+akshith312@users.noreply.github.com>
2025-11-26 18:22:01 -0500

    Time: 32 ms (32.28%) | Memory: 52.2 MB (5.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,35 +4,42 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        #brute force
+        # #brute force
         s = ''.join(c for c in s if c.isalnum())
         s = s.lower()
-        # s1 = s[::-1]
-        # s1 = s1.lower()
+        # # s1 = s[::-1]
+        # # s1 = s1.lower()
         
-        # if s == s1:
-        #     return True
-        # return False
+        # # if s == s1:
+        # #     return True
+        # # return False
 
-        l = 0
-        r = len(s) -1
+        # l = 0
+        # r = len(s) -1
 
-        while l<r:
-            while l<r and not self.alphaNum(s[l]):
-                l+=1
-            while r>l and not self.alphaNum(s[r]):
-                r-=1
-            if s[l].lower() != s[r].lower():
+        # while l<r:
+        #     while l<r and not self.alphaNum(s[l]):
+        #         l+=1
+        #     while r>l and not self.alphaNum(s[r]):
+        #         r-=1
+        #     if s[l].lower() != s[r].lower():
+        #         return False
+        #     l+=1
+        #     r-=1
+        # return True
+
+
+    # def alphaNum(self,c):
+    #     return(ord('A') <= ord(c) <= ord('Z') or 
+    #             ord('a') <= ord(c) <= ord('z') or
+    #             ord('0') <= ord(c) <= ord('9'))
+        def revstr(s, start, end):
+            if start>end:
+                return True
+            if s[start]!=s[end]:
                 return False
-            l+=1
-            r-=1
-        return True
-
-
-    def alphaNum(self,c):
-        return(ord('A') <= ord(c) <= ord('Z') or 
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
+            return revstr(s, start+1, end-1)
+        return revstr(s, 0, len(s)-1)
 
 
         
