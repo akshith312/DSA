2d9b9c9d269f3b9602085642ccafa4ea16ba638b
Akshith <33996844+akshith312@users.noreply.github.com>
2025-12-03 22:30:10 -0500

    Time: 1 ms (72.95%) | Memory: 14 MB (8.84%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: int
         """
         ptr1 = 1
-        count = 0
-        for ptr2 in range(1, len(nums)):
-            if nums[ptr2] != nums[ptr2-1]:
-                nums[ptr1] = nums[ptr2]
-                ptr1+=1
-        return ptr1
 
+        for i in range(1,len(nums)):
+            if nums[i] != nums[ptr1-1]:
+                nums[ptr1] = nums[i]
+                ptr1+=1
+        return ptr1     
+        
         
         
         
