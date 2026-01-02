97566dbb9074e44232b65f868b3692517328a3b0
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-01 20:05:41 -0500

    Time: 0 ms (100.00%) | Memory: 13 MB (77.28%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,10 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-       
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        #BRUTE FORCE
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+        # return []
+
+        hashmap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[n] = i
         return []
+
+
+
+
+
         
