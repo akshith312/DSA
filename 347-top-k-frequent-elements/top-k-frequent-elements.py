fc4ad96a4432a2e8ab5727a6cba7ac0604377131
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-07 22:52:32 -0500

    Time: 7 ms (85.56%) | Memory: 14.3 MB (74.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,45 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
-        # """
-        # Brute force
-        # count = {} 
-        
-        # for num in nums:
-        #     count[num] = 1 + count.get(num,0)
-        
-        # arr = []
-
-        # for num, cnt in count.items():
-        #     arr.append([num,cnt])
-        # arr.sort()
-
-
-        # res = []
-
-        # while len(res)<k:
-        #     res.append(arr.pop()[1])
-        # return res
-
-        count ={}
-
-        freq = [[] for i in range(len(nums)+1)]
-
+        """
+        count = {}
         for num in nums:
-            count[num] = 1+ count.get(num,0)
-        
-        for num,cnt in count.items():
-            freq[cnt].append(num)
+            count[num] = 1 + count.get(num,0)
         
         res = []
-
-        for i in range(len(freq) -1,0,-1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
-
-
-
+        for i, n in count.items():
+            res.append([n,i])
+        res.sort()
+        
+        ans = []
+        while len(ans) < k:
+            ans.append(res.pop()[1])
+        return ans
+       
         
         
