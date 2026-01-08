f6ce3c53cfed370f91155d7b5670ed27523665e8
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-08 17:36:16 -0500

    Time: 27 ms (91.31%) | Memory: 20.4 MB (49.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,34 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        #brute force
-        # n = len(nums)
-
-        # res = [0] * n
-
-        # for i in range(n):
-        #     prod = 1
-        #     for j in range(n):
-
-        #         if i==j:
-        #             continue
-        #         prod *= nums[j] 
-        #     res[i] = prod
-        # return res
-
-        res = [1] * len(nums)
-
+        ans = [1] * len(nums)
         prefix = 1
+
         for i in range(len(nums)):
-            res[i] = prefix
+            ans[i] = prefix
             prefix *= nums[i]
         
         postfix = 1
 
-        for i in range(len(nums)-1,-1,-1):
-            res[i] *= postfix
+        for i in range(len(nums) - 1, -1, -1):
+            ans[i] *= postfix
             postfix *= nums[i]
         
-        return res
-
+        return ans
         
