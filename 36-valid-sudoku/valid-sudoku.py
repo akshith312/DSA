345dbec1fdefe4fddcc5a00a439bece1b5b5607a
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-13 17:46:09 -0500

    Time: 8 ms (47.44%) | Memory: 12.6 MB (21.45%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,49 +4,17 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        #brute force
-        # for row in range(9):
-        #     seen = set()
-        #     for i in range(9):
-        #         if board[row][i]=='.':
-        #             continue
-        #         if board[row][i] in seen:
-        #             return False
-        #         seen.add(board[row][i])
-        # for col in range(9):
-        #     seen = set()
-        #     for i in range(9):
-        #         if board[i][col]=='.':
-        #             continue
-        #         if board[i][col] in seen:
-        #             return False
-        #         seen.add(board[i][col])
-        # for square in range(9):
-        #     seen = set()
-        #     for i in range(3):
-        #         for j in range(3):
-        #             row = (square // 3) * 3 + i
-        #             col = (square % 3) * 3 + j
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
 
-        #             if board[row][col] == '.':
-        #                 continue
-        #             if board[row][col] in seen:
-        #                 return False
-        #             seen.add(board[row][col])
-        # return True
-
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        square = defaultdict(set)
-
-        for r in range(9):
-            for c in range(9):
+        for r in range(len(board)):
+            for c in range(len(board)):
                 if board[r][c] == '.':
                     continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[(r//3,c//3)]:
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3,c//3)]:
                     return False
                 cols[c].add(board[r][c])
                 rows[r].add(board[r][c])
-                square[(r//3,c//3)].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
         return True
-        
