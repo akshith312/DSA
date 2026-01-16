b5720c18b47d3274035307797d91da0532379d23
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-15 22:41:23 -0500

    Time: 10 ms (65.78%) | Memory: 15.5 MB (33.91%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,18 +6,18 @@ class Solution(object):
         :rtype: List[int]
         """
         count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num,0)
-        
         res = []
+        for i in range(len(nums)):
+            if nums[i] not in count:
+                count[nums[i]] = 1
+            count[nums[i]] = 1 + count.get(nums[i], 0)
+        
         for i, n in count.items():
             res.append([n,i])
-        res.sort()
-        
+        res = sorted(res)
         ans = []
+
         while len(ans) < k:
             ans.append(res.pop()[1])
+            
         return ans
-       
-        
-        
