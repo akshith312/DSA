e1ecf9682087bc05b8031cda021fcb9ca914fe67
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-16 20:03:29 -0500

    Time: 7 ms (97.92%) | Memory: 13.5 MB (37.82%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,44 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        # #brute force
         s = ''.join(c for c in s if c.isalnum())
         s = s.lower()
-        # # s1 = s[::-1]
-        # # s1 = s1.lower()
+        # return s
+
+        return s == s[::-1]
         
-        # # if s == s1:
-        # #     return True
-        # # return False
-
-        # l = 0
-        # r = len(s) -1
-
-        # while l<r:
-        #     while l<r and not self.alphaNum(s[l]):
-        #         l+=1
-        #     while r>l and not self.alphaNum(s[r]):
-        #         r-=1
-        #     if s[l].lower() != s[r].lower():
-        #         return False
-        #     l+=1
-        #     r-=1
-        # return True
-
-
-    # def alphaNum(self,c):
-    #     return(ord('A') <= ord(c) <= ord('Z') or 
-    #             ord('a') <= ord(c) <= ord('z') or
-    #             ord('0') <= ord(c) <= ord('9'))
-        def revstr(s, start, end):
-            if start>end:
-                return True
-            if s[start]!=s[end]:
-                return False
-            return revstr(s, start+1, end-1)
-        return revstr(s, 0, len(s)-1)
-
-
         
-            
-
