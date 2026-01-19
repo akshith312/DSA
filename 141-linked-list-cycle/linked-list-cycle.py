269c6f9aea8a4391d10ae24833b61553594642ea
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-19 16:10:20 -0500

    Time: 41 ms (29.56%) | Memory: 19.6 MB (62.91%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -12,15 +12,16 @@ class Solution(object):
         """
         curr = ListNode()
         curr.next = head
-        slow = fast = curr
 
-        while fast and fast.next:
-            fast = fast.next.next
+        slow = curr
+        fast = curr
+
+        while fast.next and fast.next.next:
             slow = slow.next
-
+            fast = fast.next.next
             if slow == fast:
                 return True
         return False
-
+    
 
         
