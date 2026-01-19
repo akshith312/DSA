319aa7de2cf421954e1b423e20d6cfb6944fe43c
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-19 16:46:49 -0500

    Time: 10 ms (17.37%) | Memory: 13 MB (40.93%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,22 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        #brute force
-        # for ptr1 in range(len(numbers)):
-        #     for ptr2 in range(1,len(numbers)):
-        #         if numbers[ptr1] + numbers[ptr2] == target and ptr1!=ptr2:
-        #             return [ptr1+1, ptr2+1]
-        # return []
         l = 0
         r = len(numbers) - 1
 
-        while l<r:
-            curSum = numbers[l] + numbers[r]
+        res = []
 
-            if curSum > target:
-                r-=1
-            elif curSum < target:
-                l+=1
-            else:
-                return [l+1,r+1]
-        return []
+        while l < r:
+            if numbers[l] + numbers[r] > target:
+                r = r - 1
+            if numbers[l] + numbers[r] < target:
+                l = l + 1
+            if numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+
+        return
+            
+
+
+        
