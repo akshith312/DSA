9045b8d9bcbfd8bee7c873cd12dd18c5328b6079
Akshith <33996844+akshith312@users.noreply.github.com>
2026-01-22 21:59:22 -0500

    Time: 118 ms (50.19%) | Memory: 20.8 MB (19.03%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,25 +4,18 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        #brute force
-        # res = 0
-
-        # for i in range(len(height)):
-        #     for j in range(i+1, len(height)):
-        #         res = max(res, min(height[i],height[j])*(j-i))
-        # return res
-        res = 0
+        n = len(height)
+        l = 0
+        r = n - 1
         area = 0
-
-        l = 0 
-        r = len(height) - 1
-
+        res = 0
         while l < r:
-            area = min(height[l],height[r]) * (r-l)
-            res = max(res,area)
+            area = min(height[l], height[r]) * (r-l)
+            res = max(res, area)
 
             if height[l] <= height[r]:
-                l=l+1
+                l += 1
             else:
-                r=r-1
+                r -= 1
         return res
+
