d274703c1b6647bc25083e7462e4621b077f02cf
Akshith <33996844+akshith312@users.noreply.github.com>
2026-07-25 14:27:45 -0700

    Time: 0 ms (100.00%) | Memory: 13.3 MB (16.41%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,11 +6,12 @@ class Solution(object):
         :rtype: List[int]
         """
         hashmap = {}
-
         for i in range(len(nums)):
             diff = target - nums[i]
 
             if diff in hashmap:
                 return [hashmap[diff], i]
             hashmap[nums[i]] = i
+
         return hashmap
+       
