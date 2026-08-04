367739d0016409a358b7fadf81ac0b685ff79733
Akshith <33996844+akshith312@users.noreply.github.com>
2026-08-04 15:55:16 -0700

    Time: 23 ms (50.10%) | Memory: 14.6 MB (8.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,16 +8,16 @@ class Solution(object):
         map1 = {}
         map2 = {}
 
-        if len(s) != len(t):
+        if len(s)!=len(t):
             return False
-        
+
         for i in range(len(s)):
-            map1[s[i]] = 1 + map1.get(s[i],0)
-            map2[t[i]] = 1 + map2.get(t[i],0)
+            map1[s[i]] = 1 + map1.get(s[i], 0)
+        for j in range(len(t)):
+            map2[t[j]] = 1 + map2.get(t[j], 0)
         
         for c in map1:
             if map1[c] != map2.get(c,0):
                 return False
         return True
-            
        
